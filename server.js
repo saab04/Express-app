f5ff2001c65239fafb17891396c7ffc21b38a588
Alexander Brasegård <alexbrasegard@gmail.com>
2026-08-20 01:54:28 +0200

    MongoDB setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import path from "path"
 import { fileURLToPath } from "url";
 import "dotenv/config";
+import mongoose from "mongoose"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
@@ -10,10 +11,13 @@ const __dirname = path.dirname(__filename)
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URI).then(console.log("Connected to database sucessfully!"));
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+
+
 
 app.listen(PORT, () => {
     console.log(`Server runs on http://localhost:${PORT}`)
