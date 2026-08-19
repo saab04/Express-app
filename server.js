b6bc0fefc5be0e7f9e986ba9e3c7e39e87e97f61
Alexander Brasegård <alexbrasegard@gmail.com>
2026-08-19 23:41:50 +0200

    Express setup

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import path from "path"
+import { fileURLToPath } from "url";
+import "dotenv/config";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+    console.log(`Server runs on http://localhost:${PORT}`)
+})
+
+
+
